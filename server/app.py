2dca8cc8fcd93a5e277dfc4c1953fe5aefd8617f
calvin-arieri <morebucalvin@gmail.com>
2023-07-11 03:03:44 -0700

    completed assignment

--- server/app.py
+++ server/app.py
@@ -48,10 +48,43 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+    def post(self):
+        data=request.get_json()
+        user = User.query.filter(User.username==data.get("username")).first()
+        session['user_id'] = user.id
+        return{
+            "id":user.id,
+            "username":user.username,
+            "articles":user.articles,
+        }, 200
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
+    
+class CheckSession(Resource):
+
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return{
+                "id":user.id,
+                "username":user.username,
+                "articles":user.articles,
+            }, 200
+        else:
+            return {}, 401
+
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Logout, '/logout')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
